0faa7a4ef5d30b82cd367ffaab3755a6b82855a1
balliuera <balliuera@gmail.com>
2015-01-21 01:40:16 +0100

    added line chart

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 var pie = document.getElementById("engineering-workforce").getContext("2d");
 var engineeringWorkforceChart = new Chart(pie).Pie(engineeringWorkforce);
 
-//try to follow this style
-var waterOnEarth = new Chart(document.getElementById("water").getContext("2d"))
-                        .Pie(waterData);
+var line= document.getElementById("professions-line").getContext("2d");
+var professions = new Chart(line).Line(professionsData);
--- js/data.js
+++ js/data.js
@@ -13,17 +13,30 @@ var engineeringWorkforce = [
     }
 ];
 
-var waterData = [
-    {
-        value: 75,
-        color: "rgba(0,0,255,1)",
-        highlight: "rgba(0,0,255,0.6)",
-        label: "Water"
-    },
-    {
-        value: 25,
-        color: "rgba(244,164,96,1)",
-        highlight: "rgba(244,164,96,0.6)",
-        label: "Land"
-    }
-];
+var professionsData = {
+    labels: ["App Development","Electrical Engineering","Aeronautical Engineering", "Computer Programming","Babysitting", "Hairdressing","Nursery","Teaching"],
+    datasets: [
+        {
+            label: "Women",
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: [1,1,8,10,93,89,48]
+        },
+        {
+            label: "Men",
+            fillColor: "rgba(150,150,150,0.2)",
+            strokeColor: "rgba(150,150,150,1)",
+            pointColor: "rgba(150,150,150,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(150,150,150,1)",
+            data: [99,99,92,90,7,11,52]
+        }
+    ]
+
+
+};
