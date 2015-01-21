a7037274ce6bd156805e134a8279b7e94747b197
aziflaj <aldoziflaj95@gmail.com>
2015-01-20 18:15:52 -0800

    added other Chart for land-water percentage

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
 var pie = document.getElementById("engineering-workforce").getContext("2d");
+var engineeringWorkforceChart = new Chart(pie).Pie(engineeringWorkforce);
 
-var myNewChart = new Chart(pie).Pie(engineeringWorkforce);
+//try to follow this style
+var waterOnEarth = new Chart(document.getElementById("water").getContext("2d"))
+                        .Pie(waterData);
--- js/data.js
+++ js/data.js
@@ -1,14 +1,29 @@
 var engineeringWorkforce = [
     {
         value: 7,
-        color:"#F00",
-        highlight: "#FF5A5E",
+        color: "rgba(255,0,0,1)",
+        highlight: "rgba(255,0,0,0.6)",
         label: 'Females'
     },
     {
         value: 93,
-        color: '#0F0',
-        highlight: '#0F8',
+        color: 'rgba(0,255,0,1)',
+        highlight: 'rgba(0,255,0,0.6)',
         label: 'Males'
+    }
+];
+
+var waterData = [
+    {
+        value: 75,
+        color: "rgba(0,0,255,1)",
+        highlight: "rgba(0,0,255,0.6)",
+        label: "Water"
+    },
+    {
+        value: 25,
+        color: "rgba(244,164,96,1)",
+        highlight: "rgba(244,164,96,0.6)",
+        label: "Land"
     }
 ];
