c0e932face632962101653ed05d927138f0b83fc
aziflaj <aldoziflaj95@gmail.com>
2015-01-21 16:22:19 -0800

    added personalized colors for men and women

--- js/data.js
+++ js/data.js
@@ -1,14 +1,14 @@
 var engineeringWorkforce = [
     {
         value: 7,
-        color: "rgba(255,0,0,1)",
-        highlight: "rgba(255,0,0,0.6)",
+        color: "rgba(217,17,107,1)",
+        highlight: "rgba(217,17,107,0.6)",
         label: 'Females'
     },
     {
         value: 93,
-        color: 'rgba(0,255,0,1)',
-        highlight: 'rgba(0,255,0,0.6)',
+        color: 'rgba(54,23,179,1)',
+        highlight: 'rgba(54,23,179,0.6)',
         label: 'Males'
     }
 ];
@@ -18,22 +18,22 @@ var professionsData = {
     datasets: [
         {
             label: "Women",
-            fillColor: "rgba(220,220,220,0.2)",
-            strokeColor: "rgba(220,220,220,1)",
-            pointColor: "rgba(220,220,220,1)",
+            fillColor: "rgba(217,17,107,0.2)",
+            strokeColor: "rgba(217,17,107,1)",
+            pointColor: "rgba(217,17,107,1)",
             pointStrokeColor: "#fff",
             pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(220,220,220,1)",
+            pointHighlightStroke: "rgba(217,17,107,1)",
             data: [1,1,8,10,93,89,48]
         },
         {
             label: "Men",
-            fillColor: "rgba(150,150,150,0.2)",
-            strokeColor: "rgba(150,150,150,1)",
-            pointColor: "rgba(150,150,150,1)",
+            fillColor: "rgba(54,23,179,0.2)",
+            strokeColor: "rgba(54,23,179,1)",
+            pointColor: "rgba(54,23,179,1)",
             pointStrokeColor: "#fff",
             pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(150,150,150,1)",
+            pointHighlightStroke: "rgba(54,23,179,1)",
             data: [99,99,92,90,7,11,52]
         }
     ]
@@ -44,22 +44,22 @@ var onlineGames= {
     datasets: [
         {
             label: "Women",
-            fillColor: "rgba(220,220,220,0.2)",
-            strokeColor: "rgba(220,220,220,1)",
-            pointColor: "rgba(220,220,220,1)",
+            fillColor: "rgba(217,17,107,0.2)",
+            strokeColor: "rgba(217,17,107,1)",
+            pointColor: "rgba(217,17,107,1)",
             pointStrokeColor: "#fff",
             pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(220,220,220,1)",
+            pointHighlightStroke: "rgba(217,17,107,1)",
             data: [45,11,3,60]
         },
         {
             label: "Men",
-            fillColor: "rgba(150,150,150,0.2)",
-            strokeColor: "rgba(150,150,150,1)",
-            pointColor: "rgba(150,150,150,1)",
+            fillColor: "rgba(54,23,179,0.2)",
+            strokeColor: "rgba(54,23,179,1)",
+            pointColor: "rgba(54,23,179,1)",
             pointStrokeColor: "#fff",
             pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(150,150,150,1)",
+            pointHighlightStroke: "rgba(54,23,179,1)",
             data: [55,89,97,40]
         }
     ]
@@ -73,25 +73,23 @@ var womenData=
 
     {
         label: "Women",
-        fillColor: "rgba(200,200,200,1)",
-        strokeColor: "rgba(250,250,250,1)",
-        pointColor: "rgba(220,220,220,0.5)",
-        pointHighlightFill: "rgba(200,200,200,0.2)",
-        pointHighlightStroke: "rgba(250,250,250,0.2)",
-        pointStrokeColor: "rgba(150,150,150,1)",
+        fillColor: "rgba(217,17,107,1)",
+        strokeColor: "rgba(217,17,107,1)",
+        pointColor: "rgba(217,17,107,0.5)",
+        pointHighlightFill: "rgba(217,17,107,0.2)",
+        pointHighlightStroke: "rgba(217,17,107,0.2)",
+        pointStrokeColor: "rgba(217,17,107,1)",
         data: [66,50,10,1]
     },
     {
         label: "Men",
-        fillColor: "rgba(210,210,210,1)",
-        strokeColor: "rgba(260,260,260,1)",
-        pointColor: "rgba(230,230,230,0.5)",
-        pointHighlightFill: "rgba(210,210,210,0.2)",
-        pointHighlightStroke: "rgba(260,260,260,0.2)",
-        pointStrokeColor: "rgba(160,160,160,1)",
+        fillColor: "rgba(54,23,179,1)",
+        strokeColor: "rgba(54,23,179,1)",
+        pointColor: "rgba(54,23,179,0.5)",
+        pointHighlightFill: "rgba(54,23,179,0.2)",
+        pointHighlightStroke: "rgba(54,23,179,0.2)",
+        pointStrokeColor: "rgba(54,23,179,1)",
         data: [34,50,90,99]
-
-
     }
    ]
-}
+};
